4e3e3f2d4d25dee55a9bfcf2e353c0eddc64cdd0
angela-vargas <u01a7094@lacaixa.es>
2025-04-14 12:02:36 +0200

    Updating firebase

--- www/js/firebaseConfig.js
+++ www/js/firebaseConfig.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDis_-Swsd9sVlWHfKWDOqq4dCPzCole6Y",
-  authDomain: "martayalvarowedding.firebaseapp.com",
-  projectId: "martayalvarowedding",
-  storageBucket: "martayalvarowedding.firebasestorage.app",
-  messagingSenderId: "510769567699",
-  appId: "1:510769567699:web:561a0fb620505893738126"
+  apiKey: "AIzaSyDh5xz8XtPBt3OwkjXsyTbTObtkLPn2HYc",
+  authDomain: "soniayalvarowedding.firebaseapp.com",
+  projectId: "soniayalvarowedding",
+  storageBucket: "soniayalvarowedding.firebasestorage.app",
+  messagingSenderId: "565923232826",
+  appId: "1:565923232826:web:d26f724d93797947e88903",
+  measurementId: "G-49VTZRQE56"
 };
+
 
 
 // Initialize Firebase
