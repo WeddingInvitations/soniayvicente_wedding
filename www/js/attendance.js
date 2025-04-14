281626b7159ad554e3dfe602775667e272380f9c
O001830 <fernando.aguilar.santos@bbva.com>
2025-04-14 13:23:12 +0200

    change form

--- www/js/attendance.js
+++ www/js/attendance.js
@@ -159,7 +159,7 @@ document.getElementById('attendance-form').addEventListener("submit", function (
   var allergies = document.getElementById("alergias").value;
   var attendance = document.getElementById("acompanadoSi").checked;
   var bus = document.getElementById("bus");
-  var bus = busElement.value;
+  var bus = bus.value;
   var size = document.getElementById("talla");
 
   console.log("AUTOBÚS: " + bus);
